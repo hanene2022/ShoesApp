aa578fb22cb30d1563fb728402131803523b3478
hanenedevfull <hanenedevfulljs@gmail.com>
2024-02-14 14:51:34 +0100

    responsive mobile menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,35 @@
+import { TbShoppingBag } from "react-icons/tb"; 
 
 import NikeLogo  from "../assets/nike-logo.svg?react";
 import {RxHamburgerMenu} from "react-icons/rx";
+import { useState } from "react";
 const ROUTES =["Home","About","Services","Princing", "Contact"]
 export  function Nav() {
+  const [isMobileMenuShow,setIsMobileMenuShow]= useState(false)
   return (
     <nav className="flex  flex-wrap item-center justify-between">
       {/* logo */}
       <a href="#">  <NikeLogo className="h-20 w-20"/></a>
       {/* button */}
-  <button className=" p-2 rounded-lg focus:ring-2 focus:ring-gray-100"><RxHamburgerMenu size={25}/></button>
+  <button onClick= {()=>setIsMobileMenuShow(!isMobileMenuShow)} 
+  
+  className="  lg:hidden p-2 rounded-lg focus:ring-2 focus:ring-gray-200"><RxHamburgerMenu size={25}/></button>
    {/* menu list */}
-   <div className="w-full">
+   <div className={`${
+    isMobileMenuShow === false &&"hidden"
+   }
+    w-full lg:w-auto lb:block`}>
     
-    <ul className="  rounded-lg  p-4 border border-gray-100 bg-gray-50 text-lg">
+    <ul className="lg:space-x-8 flex 
+    flex-col 
+    lg:flex-row
+     rounded-lg 
+      p-4 border
+       border-gray-100
+     bg-gray-50 
+     text-lg
+     lg:bg-transparent
+     lg boerder-none">
       {ROUTES.map((route,i)=>{
     
       return (
@@ -21,7 +38,18 @@ export  function Nav() {
         {route}</li>);
     })}</ul>
    </div>
+{/* cart button */}
 
+
+<div className="fixed bottom-4
+left-4 lg:static">
+<div className="rounded-full
+ bg-white shadow-md
+ flex-center
+ h-12 w-12">
+<TbShoppingBag />
+</div>
+</div>
 
     </nav>
   )
